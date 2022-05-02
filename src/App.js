3f4161b8700a80fbf6c32070d4ddca370315e844
joriann-marie <60859583+joriann-marie@users.noreply.github.com>
2022-05-02 18:24:43 -0500

    Major update haul

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
-import logo from './logo.svg';
+//images
+import windowslogo from './assets/img/logos/windows.png';
+import maclogo from './assets/img/logos/mac-os-logo.png';
+import xboxlogo from './assets/img/logos/xbox-logo.png';
+import game_logo from './assets/img/Title_logo.png';
+
+//gifs
+import peck from './assets/img/peck.gif';
+import tornado from './assets/img/tornado.gif';
+import camera from './assets/img/camera.gif';
+import mech_combo from './/assets/img/mech-combo.gif'
+
+
+//download files
+import windows_dl from './assets/downloads/windows-build.zip';
+import mac_dl from './assets/downloads/mac-build.zip';
+import xbox_dl from './assets/downloads/xbox-build.zip';
+
+
 import './App.css';
 
 function App() {
@@ -6,7 +24,7 @@ function App() {
     <div className="App">
         <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
           <div class="container">
-              <a class="navbar-brand" href="#page-top"><img src="assets/img/navbar-logo.svg" alt="..." /></a>
+              <a class="navbar-brand" href="#page-top"><img src={game_logo} alt="..." /></a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                   Menu
                   <i class="fas fa-bars ms-1"></i>
@@ -25,9 +43,9 @@ function App() {
       
       <header class="masthead">
           <div class="container">
-              <div class="masthead-subheading">Raven Studios</div>
-              <div class="masthead-heading text-uppercase">Year of The Raven</div>
-              <a class="btn btn-primary btn-xl text-uppercase" href="#services">Download</a>
+              <img class="center"src={game_logo} alt="logo" width="1000"/>
+              <div class="masthead-subheading">Blackbird Studios</div>
+
           </div>
       </header>
       
@@ -39,28 +57,25 @@ function App() {
               </div>
               <div class="row text-center">
                   <div class="col-md-4">
-                      <span class="fa-stack fa-4x">
-                          <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                          <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
-                      </span>
                       <h4 class="my-3">Windows</h4>
                       <p class="text-muted">Click for Windows 64-bit Download.</p>
+                      <a href={windows_dl} download>
+                        <img src={windowslogo} alt="PC Download" width="142" height="142"/>
+                      </a>
                   </div>
                   <div class="col-md-4">
-                      <span class="fa-stack fa-4x">
-                          <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                          <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
-                      </span>
                       <h4 class="my-3">Mac</h4>
                       <p class="text-muted">Click for Mac 64-bit download</p>
+                      <a href={mac_dl} download>
+                        <img src={maclogo} alt="Mac Download" width="142" height="142"/>
+                      </a>
                   </div>
                   <div class="col-md-4">
-                      <span class="fa-stack fa-4x">
-                          <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                          <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
-                      </span>
                       <h4 class="my-3">Xbox</h4>
                       <p class="text-muted">Click for Xbox Download.</p>
+                      <a href={xbox_dl} download>
+                        <img src={xboxlogo} alt="Xbox Download" width="142" height="142"/>
+                      </a>
                   </div>
               </div>
           </div>
@@ -79,10 +94,62 @@ function App() {
       <section class="page-section" id="about">
           <div class="container">
               <div class="text-center">
-                  <h2 class="section-heading text-uppercase">About</h2>
-                  <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    <h2 class="section-heading text-uppercase">About</h2>
+                    
+                    {/* High Concept */}
+                    <h4 class="section-heading text-uppercase">High Concept</h4>
+                    <p class="section-heading text-muted">The Emperor decrees the need for a 13th divine animal to be added as a Zodiac. You play as Corvus, a blackbird who decides to shoot their shot for the title. However, you first must gain the original 12 Zodiac animals' approval by solving each of their unique puzzles and earning stars to form your constellation. With the help of your wind powers, make your way through the cloudy realm of the gods in order to ascend to divinity!<b></b></p>
+                    
+                    {/* Trailer */}
+                    <h4 class="section-heading text-uppercase">Game Trailer</h4>
+
+                    {/* Mechanics */}
+                    <h4 class="section-heading text-uppercase">Mechanics</h4>
+                    <div class="row text-center text-muted">
+                        <div class="col-sm">
+                            <h5>Peck</h5>
+                            <img src={peck} alt="Peck gif" height="240"/>
+                            <p>The peck ability allows the player to move boxes and interact with other objects and NPCs.</p>
+                        </div>
+                        <div class="col-sm">
+                            <h5 class="text-muted ">Tornado</h5>
+                            <img src={tornado} alt="Tornado gif" height="240"/>
+                            <p>The player can shoot out a gust of wind which creates a tornado. The player and boxes can also float on this tornado.</p>
+                        </div>
+                        <div class="col-sm">
+                            <h5 class="text-muted">Camera</h5>
+                            <img src={camera} alt="Camera gif" height="240"/>
+                            <p>The player can shift camera in 90 degree increments to view puzzles at different angles.</p>
+                        </div>
+                    </div>
+
+                    {/* Dynamics */}
+                    <h4 class="section-heading text-uppercase">Dynamics</h4>
+                    <div class="container">
+                        <div class="row text-center text-muted">
+                            <div class="col-md-4">
+                                <h5>Mechanic Combination</h5>
+                                <img src={mech_combo} alt="mechanic combination gif" height="240"/>
+                                <p>The player can combine the tornado and peck to solve different puzzles.</p>
+                            </div>
+                            <div class="col-md-4">
+                                <h5 class="text-muted ">Tornado</h5>
+                                <img src={tornado} alt="Tornado gif" height="240"/>
+                                <p>The player can shoot out a gust of wind which creates a tornado. The player and boxes can also float on this tornado.</p>
+                            </div>
+                            <div class="col-md-4">
+                                <h5 class="text-muted">Camera</h5>
+                                <img src={camera} alt="Camera gif" height="240"/>
+                                <p>The player can shift camera in 90 degree increments to view puzzles at different angles.</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    
+                    <h4 class="section-heading text-uppercase">Aesthetic</h4>
               </div>
-              <ul class="timeline">
+              
+              {/* <ul class="timeline">
                   <li>
                       <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/1.jpg" alt="..." /></div>
                       <div class="timeline-panel">
@@ -134,7 +201,7 @@ function App() {
                           </h4>
                       </div>
                   </li>
-              </ul>
+              </ul> */}
           </div>
       </section>
       
